be14a9cf5ced05432430c7e203cc9cacd9f14862
zd <267180680@qq.com>
2019-05-03 15:04:00 +0800

    修改账号层加载和保存逻辑

--- server/lualib/db/account.lua
+++ server/lualib/db/account.lua
@@ -1,6 +1,6 @@
 local constant = require "constant"
-local srp = require "srp"
-
+--local srp = require "srp"
+local uuid = require "uuid"
 
 local account = {}
 local connection_handler
@@ -9,37 +9,49 @@ function account.init (ch)
 	connection_handler = ch
 end
 
-local function make_key (name)
+local function make_key (account_name, platform)
+    local name = platform .. "_" .. account_name
 	return connection_handler (name), string.format ("user:%s", name)
 end
 
-function account.load (name)
-	assert (name)
+function account.load (account_name, password, channel, platform)
+    print("account_name=", account_name)
+    print("password=", password)
+    print("channel=", channel)
+    print("platform=", platform)
+	assert (account_name and password and channel and platform, "invalid account parameters to load" )
 
-	local acc = { name = name }
+	local acc = {account = account_name, password = password, channel = channel, platform = platform}
 
-	local connection, key = make_key (name)
+	local connection, key = make_key (account_name, platform)
 	if connection:exists (key) then
-		acc.id = connection:hget (key, "account")
-		acc.salt = connection:hget (key, "salt")
-		acc.verifier = connection:hget (key, "verifier")
+        local pwd = connection:hget (key, "password")
+        if pwd == password then
+            acc.account_id = connection:hget (key, "account_id")
+        end
+
 	else
-		acc.salt, acc.verifier = srp.create_verifier (name, constant.default_password)
+        assert (account_name and #account_name < 32 and password and #password < 32 and channel and #channel < 16 and platform and #platform < 16, "invalid argument to create")
+        local account_id = uuid.gen()
+        assert (connection:hsetnx (key, "account_id", account_id) ~= 0, "create account failed")
+        assert (connection:hmset (key, "account", account_name, "password", password, "channel", channel, "platform", platform, "create_time", os.time()) ~= 0, "save account parameters failed")
+
+        acc.account_id = account_id
 	end
 
 	return acc
 end
 
-function account.create (id, name, password)
-	assert (id and name and #name < 24 and password and #password < 24, "invalid argument")
-	
-	local connection, key = make_key (name)
-	assert (connection:hsetnx (key, "account", id) ~= 0, "create account failed")
-
-	local salt, verifier = srp.create_verifier (name, password)
-	assert (connection:hmset (key, "salt", salt, "verifier", verifier) ~= 0, "save account verifier failed")
-
-	return id
-end
+--function account.create (id, name, password)
+--	assert (id and name and #name < 64 and password and #password < 64, "invalid argument")
+--	
+--	local connection, key = make_key (name)
+--	assert (connection:hsetnx (key, "account", id) ~= 0, "create account failed")
+--
+--	local salt, verifier = srp.create_verifier (name, password)
+--	assert (connection:hmset (key, "salt", salt, "verifier", verifier) ~= 0, "save account verifier failed")
+--
+--	return id
+--end
 
 return account
